e00665992d1a52f132e373812dc023dd6857e9ce
Zakaria Mansouri <zakman.dev@gmail.com>
2024-11-14 21:08:40 +0100

    setup tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 use proc_macro::TokenStream;
+use quote::quote;
 
 /// Nest struct definitions with minimal syntax changes.
 #[proc_macro_attribute]
 pub fn nest_struct(_attr: TokenStream, _item: TokenStream) -> TokenStream {
-    TokenStream::new()
+    let expanded = quote! {
+        struct Expanded;
+    };
+
+    TokenStream::from(expanded)
 }
---
+++ tests/expand/hello_world.rs
@@ -0,0 +1,5 @@
+#[macro_use]
+extern crate nest_struct;
+
+#[nest_struct]
+struct HelloWorld;
