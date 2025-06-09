ff02f512972159d7096807c2590f9ca991dd1b71
Zakaria Mansouri <zakman.dev@gmail.com>
2025-06-09 16:41:37 +0200

    setup tests for capture parsing errors

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,4 +1,20 @@
 #[test]
-pub fn expand() {
-    macrotest::expand("tests/cases/**/*.rs");
+pub fn expand_struct() {
+    macrotest::expand("tests/cases/struct/*.rs");
+}
+
+#[test]
+fn expand_enum() {
+    macrotest::expand("tests/cases/enum/*.rs");
+}
+
+#[test]
+fn expand_block() {
+    macrotest::expand("tests/cases/block/*.rs");
+}
+
+#[test]
+fn parse_error() {
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/cases/parse_error/*.rs");
 }
